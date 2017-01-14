581dff740a8283cde39031801b3a73b3a5d5cb2d
Fangstyle <fangstyles@163.com>
2017-01-14 10:50:37 +0800

    自定义jsonp异步请求

---
+++ app/components/httpServer.js
@@ -0,0 +1,21 @@
+/**
+ * Created by baocheng on 2017/1/14.
+ */
+'use strict';
+(function (angular) {
+	angular.module("myApp.service.httpServer",[]).service("httpServer",["$window","$document",function ($window,$document)
+	{
+		this.jsonP = function (url,data,callback) {
+			var querystring = url.indexOf('?') == -1 ? '?' : '&';
+			for (var key in data){
+				querystring += key+"="+data[key]+"&";
+			}
+			var callbackName = "fang_jsonp_"+Math.random().toString().replace(".","");
+			querystring+= "callback="+callbackName;
+			$window[callbackName] = callback;
+			var scriptElement = $document[0].createElement('script');
+			scriptElement[src]= url+querystring;
+			$document[0].body.appendChild(scriptElement);
+		}
+	}]);
+})(angular)
